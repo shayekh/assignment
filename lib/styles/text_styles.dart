0de19b08f6531bec495001dd7a538fe626849cba
shayekh <shayekhebnemizan@gmail.com>
2022-04-07 01:00:01 +0600

    TextStyle

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+const bigText = TextStyle(
+  fontSize: 80,
+  fontWeight: FontWeight.bold
+);
+
+const labelText = TextStyle(
+    fontSize: 35,
+    letterSpacing: 1.2
+);
+
+const subText = TextStyle(
+    fontSize: 25,
+);
