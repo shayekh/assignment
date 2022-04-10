9f7086f3fcac244feaf60149684576905f4c218d
shayekh <shayekhebnemizan@gmail.com>
2022-04-11 04:41:48 +0600

    Consumer Provider

--- lib/pages/current_weather_page.dart
+++ lib/pages/current_weather_page.dart
@@ -2,6 +2,7 @@ import 'package:Assisgnment/providers/weather_provider.dart';
 import 'package:Assisgnment/styles/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:Assisgnment/utils/weather_utils.dart';
 
 class CurrentWeatherPage extends StatelessWidget {
 
@@ -17,7 +18,13 @@ class CurrentWeatherPage extends StatelessWidget {
                     children: <Widget>[
                       Text('${provider.currentData.name}, ${provider.currentData.sys.country}', style: labelText,),
                       Text('${provider.currentData.main.temp}\u00B0C', style: bigText,),
-                      Text('Feels like: ${provider.currentData.main.feelsLike}\u00B0C', style: subText,)
+                      Text('Feels like: ${provider.currentData.main.feelsLike}\u00B0C', style: subText,),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          (getFormattedDate(provider.currentData.dt, 'hh:mm a, EEEE, MMMM dd, yyyy')),
+                        ),
+                      ),
                     ],
                   ),
       )
